be2f1f296170a3eac68f152db4306bbed32b6704
Powersource <jacob.karlsson95@gmail.com>
2015-10-15 03:46:14 +0200

    Made the show-emojis-buttons

--- public_html/main.js
+++ public_html/main.js
@@ -1,12 +1,25 @@
 "use strict";
 $(function () {
-    console.log("hi");
-    var div = $('div');
-    div.html("");
-    $.getJSON("https://api.github.com/emojis", {}, function (json) {
-        $.each(json, function (index) {
-            //$($.parseHTML("<div>" + index.toString() + ": " +  json[index] + "</div")).appendTo(div);
-            div.append("<div>" + index.toString() + ": " + json[index] + "</div");
-        });
-    });
+	console.log("hi");
+	var mainDiv = $('div');
+	mainDiv.html("");
+	$.getJSON("https://api.github.com/emojis", {}, function (json) {
+		$.each(json, function (index) {
+			//$($.parseHTML("<div>" + index.toString() + ": " +  json[index] + "</div")).appendTo(div);
+			mainDiv.append("<div>"
+					+ "<button class=\"imageShower\">Show emoji</button>"
+					+ index.toString() + ": " + "<span>" + json[index] + "</span>"
+					+ "</div");
+		});
+		$(".imageShower").click(function () {
+			console.log("click");
+			var imgURL =
+					$(this)
+					.parent()
+					.children("span")
+					.html();
+			$(this).replaceWith("<image src=\""
+					+ imgURL + "\"/>");
+		});
+	});
 });
